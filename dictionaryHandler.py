b0cc791181e6f233abe2647dcbde5993a73c62e9
b-boechat <54223141+b-boechat@users.noreply.github.com>
2021-03-31 22:46:31 -0300

    Update addSortedToFlashcardDictionary() with print statements indicating words added.

--- dictionaryHandler.py
+++ dictionaryHandler.py
@@ -1,8 +1,8 @@
 from definitions import FLASHCARD_DICTIONARY_FULL_PATH, STAGING_FLASHCARD_DICTIONARY_FULL_PATH
 from shutil import copy2
 import os
-import re
 from bisect import bisect_left
+import colorama
 
 def addSortedToFlashcardDictionary(words, presort_dictionary=True):
     """ Merge list of words with flashcard dictionary file while keeping it sorted and with no repeated words. If presort_dictionary is set to False, requires and assumes that flashcard dictionary file is already sorted, and operates faster.
@@ -26,8 +26,9 @@ def addSortedToFlashcardDictionary(words, presort_dictionary=True):
         insertion_point = bisect_left(flashcard_dictionary_list, word)
         # If word is already in dictionary, ignore it.
         if insertion_point != len(flashcard_dictionary_list) and flashcard_dictionary_list[insertion_point] == word:
+            print(colorama.Fore.RED + "Word was already in dictionary: {}".format(word))
             continue
-        # Otherwise, append tuple with word and its insertion point to the list of insertions. This insertion point is related to the original flashcard_dictionary_list, and purposefully ignores insertions from previous iterations.
+        # Otherwise, append tuple with word and its insertion point to the list of insertions. This insertion point is related to the original flashcard_dictionary_list, not counting insertions from previous iterations.
         insertions.append((word, insertion_point))
 
     # Create new flashcard dictionary file to replace the old one.
@@ -40,6 +41,7 @@ def addSortedToFlashcardDictionary(words, presort_dictionary=True):
                 output_file.write("{}\n".format(flashcard_dictionary_list[i]))
             # Add insertion at the correct position.
             output_file.write("{}\n".format(insertion[0]))
+            print(colorama.Fore.GREEN + "Word added to dictionary: {}".format(insertion[0]))
             # Update start index, for flashcard_dictionary_list, for the next iteration, so that each element is added once and only once.
             next_start_index = insertion[1]
         # Add remaining elements from flashcard_dictionary_list after the last insertion.
