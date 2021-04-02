f654b5b4fde04d2b6b3ff5528abf128f7c978897
b-boechat <54223141+b-boechat@users.noreply.github.com>
2021-04-02 19:33:00 -0300

    Remove old code from testFunctions.py

--- testFunctions.py
+++ testFunctions.py
@@ -6,14 +6,4 @@ from utilities import *
 from ioFilesHandler import *
 from dictionaryHandler import *
 
-#formatted_sentence = r"Je sais: ça [ressembler] [énormément] à <1un> [1supplice]."
-
-#print(getRawSentenceFromFormatted(formatted_sentence))
-
-
-addSortedToFlashcardDictionary("kappa")
-addSortedToFlashcardDictionary("abelha")
-addSortedToFlashcardDictionary("ola")
-addSortedToFlashcardDictionary("banana")
-
-sortFlashcardDictionary()
+import colorama
