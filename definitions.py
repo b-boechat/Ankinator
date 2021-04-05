d6e2b5deb4ec5ae19e916d7b2956e28f7b9f0769
b-boechat <54223141+b-boechat@users.noreply.github.com>
2021-04-05 14:56:48 -0300

    Change dict_input.txt location to the dictionary folder.

--- definitions.py
+++ definitions.py
@@ -4,7 +4,8 @@ STAGING_PATH = "staging/"
 IMAGE_PATH = "C:\\Users\\berna\\AppData\\Roaming\\Anki2\\User 1\\collection.media\\"
 BACKUP_PATH = "backup/"
 FLASHCARD_DICTIONARY_FULL_PATH = "dictionary/dictionary.txt"
-FLASHCARD_DICTIONARY_INPUT_FULL_PATH = "staging/dict_input.txt"
+FLASHCARD_DICTIONARY_INPUT_FULL_PATH = "dictionary/dict_input.txt"
+WORD_SEARCH_FILE_FULL_PATH = "dictionary/word_search.txt"
 STAGING_FLASHCARD_DICTIONARY_FULL_PATH = "staging/dictionary_temp.txt"
 IPA_DICTIONARY_FULL_PATH = "ipa/fr_FR.txt"
 IMAGE_HEIGHT = 300
@@ -12,4 +13,4 @@ MAXIMUM_BEGINNING_CHARACTERS_IMAGE_FILENAME = 8
 IMAGE_FILENAME_PREFIX = "_FR_"
 
 DONT_MOVE_IMAGES = True # Useful for debugging or testing.
-DONT_UPDATE_DICTIONARY = False # Useful for debugging or testing.
+DONT_UPDATE_DICTIONARY = True # Useful for debugging or testing.
