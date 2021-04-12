b83d25db547f571ca7a52c8ddadb7cc57fc6e2cf
b-boechat <54223141+b-boechat@users.noreply.github.com>
2021-04-11 22:27:51 -0300

    Delete testFunctions.py

--- testFunctions.py
+++
@@ -1,9 +0,0 @@
-from sentenceEntryProcessor import *
-from imageHandler import *
-from addIPATranscription import *
-from definitions import *
-from utilities import *
-from ioFilesHandler import *
-from dictionaryHandler import *
-
-import colorama
