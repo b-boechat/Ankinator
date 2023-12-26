16c9785fd9405ecbb9d4d285b29816467a26ba82
b-boechat <bernardoant@poli.ufrj.br>
2023-12-26 09:33:13 -0300

    Add TTS keys folder.

--- definitions.py
+++ definitions.py
@@ -11,7 +11,7 @@ FLASHCARD_DICTIONARY_INPUT_FULL_PATH = ANKINATOR_BASE_PATH + "dictionary\\dict_i
 WORD_SEARCH_FILE_FULL_PATH = ANKINATOR_BASE_PATH + "dictionary\\word_search.txt"
 STAGING_FLASHCARD_DICTIONARY_FULL_PATH = ANKINATOR_BASE_PATH + "staging\\dictionary_temp.txt"
 IPA_DICTIONARY_FULL_PATH = ANKINATOR_BASE_PATH + "ipa\\fr_FR.txt"
-GOOGLE_TTS_CREDENTIALS_FULL_PATH = ANKINATOR_BASE_PATH + "_ankinatortts.json"
+GOOGLE_TTS_CREDENTIALS_FULL_PATH = ANKINATOR_BASE_PATH + "tts_keys\\_ankinator-4bccc5f5f492.json"
 
 ANKI_DECK = "French learning"
 ANKI_NOTE_TYPE = "French Sentence Cards"
@@ -20,5 +20,5 @@ IMAGE_HEIGHT = 300
 MAXIMUM_BEGINNING_CHARACTERS_IMAGE_FILENAME = 8
 MEDIA_FILENAME_PREFIX = "_FR_"
 
-DONT_MOVE_MEDIA = False # Useful for debugging or testing.
-DONT_UPDATE_DICTIONARY = False # Useful for debugging or testing.
+DONT_MOVE_MEDIA = True # Useful for debugging or testing.
+DONT_UPDATE_DICTIONARY = True # Useful for debugging or testing.
